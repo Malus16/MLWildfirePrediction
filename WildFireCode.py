d54c7fd2c2acced8cc44a64a6d3b17c9b5e93704
Mikkel Noyé Knudsen <mnok@tv2.dk>
2024-05-27 17:50:27 +0200

    Added comments, deleted old test code

--- WildFireCode.py
+++ WildFireCode.py
@@ -13,11 +13,12 @@ one_gridpoint = xr.concat([da[:,120,0] for _, da in data.data_vars.items()], dim
 
 one_gridpoint = one_gridpoint.to_numpy().T
 
-
+# Initial pandas dataframe that is appended to later. Initialize on land gridpoint
 all_grid_points = pd.DataFrame(data=one_gridpoint, columns=list(data.variables)[3:])
 
 
 #%%
+# Split the dataset up into parts so that the command doesn't overload memory
 gridpoints = xr.concat([da[:,:,:125] for _, da in data.data_vars.items()], dim='variable')
 
 #%%
@@ -28,34 +29,24 @@ gridpoints4 = xr.concat([da[:,:,375:] for _, da in data.data_vars.items()], dim=
 #%%
 gridpointlist = [gridpoints,gridpoints2,gridpoints3,gridpoints4]
 #%%
-
+# This is the loop creating the tabulated data by appending to the dataframe
 lats = len(data['latitude'])
 lons = len(data['longitude'])
 sst = data['sst'][0,:,:]
 var_names = list(data.variables)[3:]
+arrlist = [0]*125 + [1]*125 + [2]*125 + [3]*126
 for i in range(lats):
-    for j in range(50):
+    for j in range(lons):
+        arr = arrlist[j]
         if np.isnan(sst[i,j]) == False:
             continue
         if i == 120 and j == 0:
             continue
-        gridpoint = gridpoints[:,:,i,j].T
+        gridpoint = gridpointlist[arr][:,:,i,j - arr*125].T
         # gridpoint = xr.concat([da[:,i,j] for _, da in data.data_vars.items()], dim='variable').T
         gridpoint = pd.DataFrame(data=gridpoint.to_numpy(), columns=var_names)
         all_grid_points = pd.concat([all_grid_points,gridpoint])
 
 
-
 #%%
-gridpoints2 = xr.concat([da[:,46,45] for _, da in data.data_vars.items()], dim='variable').T
-
-
-gridpoints2 = pd.DataFrame(data=gridpoints2.to_numpy(), columns=list(data.variables)[3:])
-
-#%%
-wow2 = pd.concat([wow,gridpoints2])
-# wow2 = wow.append(gridpoints2, ignore_index=True)
-
-
-#%%
-damn = xr.concat([gridpoints, gridpoints2], dim=gridpoints.dims)
+all_grid_points.to_csv('Tabulated grid data.csv')
